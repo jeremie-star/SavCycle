daa97ade40b5fc6e7fea43ab61388d1fd756dc2e
alusamuel <s.rurangami@alustudent.com>
2025-07-12 06:20:19 +0200

    add firebase sdk configuration

---
+++ backend_application/config/firebase.js
@@ -0,0 +1,4 @@
+const admin = require("firebase-admin");
+const serviceAccount = require("../firebase-adminsdk.json");
+admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
+module.exports = admin;
